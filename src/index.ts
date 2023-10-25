068ae78763dd001fad8d83339039c5375a21739c
Cryotheus <wisedthecoder@gmail.com>
2023-10-24 22:56:01 -0400

    This is terrible; I will redo all of this :(

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,8 @@ const cardInfo: [string, string, string][] = [
 	["", "Games", ""]
 ];
 
+let glow: HTMLDivElement;
+
 function buildCards() {
 	const cardsDiv = el.get<HTMLDivElement>("cards");
 
@@ -14,4 +16,38 @@ function buildCards() {
 
 window.addEventListener('load', _ => {
 	buildCards();
+});
+
+window.addEventListener("mousemove", event => {
+	if (!glow) {
+		glow = document.getElementById("glow") as HTMLDivElement;
+
+		if (!glow)
+			return;
+	}
+
+	let glowStyle = glow.style;
+	let parent = glow.parentElement;
+
+	if (!parent)
+		return;
+	
+	let rect = parent.getBoundingClientRect();
+	let x = event.clientX - rect.x;
+	let y = event.clientY - rect.y
+
+	console.log(rect.left, rect.top)
+
+	glowStyle.left = x + "px";
+	glowStyle.top = y + "px";
+
+	//console.log(target, target?.tagName, target?.className);
+
+	//top right bottom left
+	//glowStyle.clipPath = `inset(160px 41px 78px -${x}px round 8px)`;
+});
+
+window.addEventListener("mouseenter", event => {
+	console.log("event!");
+	console.log(event.currentTarget, event.target);
 });
